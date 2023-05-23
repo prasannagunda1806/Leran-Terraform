f416e4d854d7f6a2cf6c222c6f0a0b15911ad06a
prasanna <prasanna.gunda06@gmail.com>
2023-05-23 17:06:04 +0000

    terraform first program

---
+++ test.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "sample"{
+    ami            = "ami-0b5a2b5b8f2be4ec2"
+    Instance_type  =  "t3.micro"
+    vpc_security_group_ids = ["sg-05963ecb7a99cd5e6"]
+    
+   tags  =  {
+      name = "testing"
+  }
+}
